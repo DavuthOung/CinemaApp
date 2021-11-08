4719ba9eaedafa76a141cf7e1e5cd122f3db01c2
DavuthOung <davuthlove96@gmail.com>
2021-11-08 10:41:58 +0700

    fixed home screen

--- src/screens/home/screen/index.js
+++ src/screens/home/screen/index.js
@@ -1,20 +1,14 @@
-import React ,{useState} from "react";
-import {View,Button,TextInput} from "react-native";
+import React from "react";
+import {View,Button} from "react-native";
 import { useTheme, } from "@react-navigation/native";
 
 export default function HomeScreen({ navigation }) {
-    const [count, setCount] = useState("");
     const { colors } = useTheme();
     return (
         <View style={{ flex: 1, alignItems: "center", justifyContent: "center",backgroundColor: colors.background }}>
             <Button
                 title="Detail"
                 onPress={() => navigation.navigate("Detail")}
-            />
-            <TextInput 
-                style={{height:50,width:100,borderWidth:1}}
-                value={count}
-                onChangeText={text => setCount(text)}
             />
         </View>
     );
